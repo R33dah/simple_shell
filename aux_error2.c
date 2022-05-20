8bea59cc959db049ba25c19f1ea6771cc834219a
r33edah <little.musahh@gmail.com>
2022-05-20 22:51:06 +0100

    fix

--- aux_error2.c
+++ aux_error2.c
@@ -50,4 +50,20 @@ char *error_path_126(data_shell *datash)
 	ver_str = aux_itoa(datash->counter);
 	length = _strlen(datash->av[0]) + _strlen(ver_str);
 	length += _strlen(datash->args[0]) + 24;
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(error);
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->args[0]);
+	_strcat(error, ": Permission denied\n");
+	_strcat(error, "\0");
+	free(ver_str);
+	return (error);
 }
